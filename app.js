5924df6c28ee00f731c2af82e5d42c0ab52d42ff
AnnabathinaMounika <112028867+AnnabathinaMounika@users.noreply.github.com>
2022-11-02 18:08:14 -0500

    added Wardrobe.js and pug file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var WardrobeRouter = require('./routes/Wardrobe');
 
 var app = express();
 
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Wardrobe', WardrobeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
